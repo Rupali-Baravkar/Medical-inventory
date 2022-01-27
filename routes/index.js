3e68ba9c21dcdc9fd46a34ab1919e92480d10796
csjob <csjob78@gmail.com>
2022-01-27 19:59:31 +0530

    admin user UI

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Medical inventory' });
-
+  // res.render('index', { title: 'Medical inventory' });
+  res.render('dashboard');
 });
 
 router.get('/Login', function(req, res, next){
-  res.render('Login')
-})
+  res.render('Login');
+});
+
 
 router.get('/admindash', function(req, res, next){
   res.render('admindash')
